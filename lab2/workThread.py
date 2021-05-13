eb72f6e9fc4a54b0aba1cc068e7b0d04a4ef1204
Abram Nguyen <amnguyen2@miners.utep.edu>
2021-05-13 17:37:30 -0600

    implemented locks on file transfer

--- lab2/workThread.py
+++ lab2/workThread.py
@@ -1,8 +1,12 @@
 import os, sys, socket, time
-from threading import Thread, enumerate
+from threading import Thread, enumerate, Lock
 import frameSock
 
 threadNum = 0
+
+lock = Lock()
+inUse = set()
+
 
 class workThread(Thread):
     def __init__(self, connectedSock, addr):
@@ -12,7 +16,22 @@ class workThread(Thread):
         self.connectedSock = connectedSock
         self.addr = addr
 
+    def isFileInUse(self, file_name):
+        global lock
+        global inUse
+
+        lock.acquire()
+        if file_name not in inUse:
+            inUse.add(file_name)
+            lock.release()
+            return True
+        
+        lock.release()
+        return False
+        
     def run(self):
+        global inUse
+        
         print("Worker thread running.")
         framed_sock = frameSock.frameSock(self.connectedSock)
 
@@ -35,6 +54,10 @@ class workThread(Thread):
                 os.write(fd, (framed_sock.recv_msg()).encode())
                 os.close(fd)
                 os.write(1, "File {} created.\n".format(file_name).encode())
+                
+            inUse.remove(file_name) # file has been sent successfully at this point
+
         else:
             os.write(1, "Could not complete request".encode())
+
         self.connectedSock.shutdown(socket.SHUT_WR)
